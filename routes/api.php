75819681c820311a45c4ce0d19ad68cd16513b3c
sthefanyk <sthefanykmbrl@gmail.com>
2023-08-11 18:06:37 -0300

    personalidade api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\PersonalidadeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::apiResource('/personalidades', PersonalidadeController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function(){
+    return response()->json(['menssage' => 'success']);
 });
